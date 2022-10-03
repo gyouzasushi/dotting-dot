e1509e867d3d205f3ef3211e04a4660fd308f23c
gyouzasushi <tasukuishikawa@gmail.com>
2022-10-03 16:16:14 +0900

    super gyouza

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const input = document.getElementById("input");
 const sizeBar = document.getElementById("size_bar");
+const saveButton = document.getElementById("save");
 const canvas = document.createElement("canvas");
 const svg = document.getElementById("svg");
 const ctx = canvas.getContext("2d");
 const img = new Image();
+const buf = new Array();
 const svg_h = 400;
 input.onchange = loadImage;
 function loadImage() {
@@ -29,18 +31,21 @@ function loadImage() {
         canvas.width = img.width;
         ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, 0, 0);
         const size = parseInt(sizeBar.value);
-        createUltraPixel(size);
-        console.log(size);
+        const h = Math.floor(img.height / size);
+        const w = Math.floor(img.width / size);
+        document.getElementById("size").textContent = `${h}x${w}`;
+        const len = img.height * img.width * 4;
+        while (buf.length < len)
+            buf.push(0);
+        gyouza(size);
     });
 }
-function createUltraPixel(size) {
+function gyouza(size) {
     const h = Math.floor(img.height / size) * size;
     const w = Math.floor(img.width / size) * size;
     svg.setAttribute('width', `${svg_h / h * w}`);
-    console.log(h, w);
     const imageData = ctx.getImageData(0, 0, w, h);
     const data = imageData.data;
-    const buf = new Array(h * w * 4);
     buf.fill(0);
     for (let y = 0; y < h; y += size) {
         for (let x = 0; x < w; x += size) {
@@ -73,7 +78,7 @@ function createUltraPixel(size) {
             const R = buf[i + 0];
             const G = buf[i + 1];
             const B = buf[i + 2];
-            const Y = Math.floor((0.2126 * R + 0.7152 * G + 0.0722 * B) / 52 * 52);
+            const Y = Math.floor((0.2126 * R + 0.7152 * G + 0.0722 * B) / 52) * 52;
             const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
             rect.setAttribute('y', `${y0 * D}`);
             rect.setAttribute('x', `${x0 * D}`);
@@ -84,10 +89,43 @@ function createUltraPixel(size) {
             svg.appendChild(rect);
         }
     }
-    console.log(`${h / size}x${w / size}`);
+}
+const bg = [];
+const fg = [];
+function sushi(size) {
+    const h = Math.floor(img.height / size) * size;
+    const w = Math.floor(img.width / size) * size;
+    while (svg.lastChild) {
+        svg.removeChild(svg.lastChild);
+    }
+    const D = size * svg_h / h;
+    for (let y = 0, y0 = 0; y < h; y += size, y0++) {
+        for (let x = 0, x0 = 0; x < w; x += size, x0++) {
+            const i = (y * w + x) * 4;
+            const R = buf[i + 0];
+            const G = buf[i + 1];
+            const B = buf[i + 2];
+            const id = Math.floor((0.2126 * R + 0.7152 * G + 0.0722 * B) / 52);
+            const Y = id * 52;
+            const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            rect.setAttribute('y', `${y0 * D}`);
+            rect.setAttribute('x', `${x0 * D}`);
+            rect.setAttribute('height', `${D}`);
+            rect.setAttribute('width', `${D}`);
+            rect.setAttribute('fill', `rgb(${Y}, ${Y}, ${Y})`);
+            rect.setAttribute('stroke', `rgb(${Y}, ${Y}, ${Y})`);
+            svg.appendChild(rect);
+        }
+    }
 }
 sizeBar.onchange = () => {
     const size = parseInt(sizeBar.value);
-    createUltraPixel(size);
-    console.log(size);
+    gyouza(size);
+    const h = Math.floor(img.height / size);
+    const w = Math.floor(img.width / size);
+    document.getElementById("size").textContent = `${h}x${w}`;
+};
+saveButton.onclick = () => {
+    // const size = parseInt(sizeBar.value);
+    // sushi(size);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 const input = <HTMLInputElement>document.getElementById("input")!;
 const sizeBar = <HTMLInputElement>document.getElementById("size_bar")!;
+const saveButton = <HTMLButtonElement>document.getElementById("save")!;
 const canvas = document.createElement("canvas");
 const svg = document.getElementById("svg")!;
 const ctx = canvas.getContext("2d")!;
 const img = new Image();
+const buf = new Array<number>();
 const svg_h = 400;
 
 input.onchange = loadImage;
@@ -20,18 +22,20 @@ async function loadImage() {
     ctx?.drawImage(img, 0, 0);
 
     const size = parseInt(sizeBar.value);
+    const h = Math.floor(img.height / size);
+    const w = Math.floor(img.width / size);
+    document.getElementById("size")!.textContent = `${h}x${w}`;
+    const len = img.height * img.width * 4;
+    while (buf.length < len) buf.push(0);
     gyouza(size);
-    console.log(size);
 }
 
 function gyouza(size: number) {
     const h = Math.floor(img.height / size) * size;
     const w = Math.floor(img.width / size) * size;
     svg.setAttribute('width', `${svg_h / h * w}`);
-    console.log(h, w);
     const imageData = ctx.getImageData(0, 0, w, h);
     const data = imageData.data;
-    const buf = new Array<number>(h * w * 4);
     buf.fill(0);
     for (let y = 0; y < h; y += size) {
         for (let x = 0; x < w; x += size) {
@@ -54,6 +58,7 @@ function gyouza(size: number) {
             }
         }
     }
+
     while (svg.lastChild) {
         svg.removeChild(svg.lastChild);
     }
@@ -64,7 +69,7 @@ function gyouza(size: number) {
             const R = buf[i + 0];
             const G = buf[i + 1];
             const B = buf[i + 2];
-            const Y = Math.floor((0.2126 * R + 0.7152 * G + 0.0722 * B) / 52 * 52);
+            const Y = Math.floor((0.2126 * R + 0.7152 * G + 0.0722 * B) / 52) * 52;
             const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
             rect.setAttribute('y', `${y0 * D}`);
             rect.setAttribute('x', `${x0 * D}`);
@@ -75,11 +80,46 @@ function gyouza(size: number) {
             svg.appendChild(rect);
         }
     }
-    console.log(`${h / size}x${w / size}`);
+}
+
+const bg = [];
+const fg = [];
+function sushi(size: number) {
+    const h = Math.floor(img.height / size) * size;
+    const w = Math.floor(img.width / size) * size;
+    while (svg.lastChild) {
+        svg.removeChild(svg.lastChild);
+    }
+    const D = size * svg_h / h;
+    for (let y = 0, y0 = 0; y < h; y += size, y0++) {
+        for (let x = 0, x0 = 0; x < w; x += size, x0++) {
+            const i = (y * w + x) * 4;
+            const R = buf[i + 0];
+            const G = buf[i + 1];
+            const B = buf[i + 2];
+            const id = Math.floor((0.2126 * R + 0.7152 * G + 0.0722 * B) / 52);
+            const Y = id * 52;
+            const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            rect.setAttribute('y', `${y0 * D}`);
+            rect.setAttribute('x', `${x0 * D}`);
+            rect.setAttribute('height', `${D}`);
+            rect.setAttribute('width', `${D}`);
+            rect.setAttribute('fill', `rgb(${Y}, ${Y}, ${Y})`);
+            rect.setAttribute('stroke', `rgb(${Y}, ${Y}, ${Y})`);
+            svg.appendChild(rect);
+        }
+    }
 }
 
 sizeBar.onchange = () => {
     const size = parseInt(sizeBar.value);
     gyouza(size);
-    console.log(size);
+    const h = Math.floor(img.height / size);
+    const w = Math.floor(img.width / size);
+    document.getElementById("size")!.textContent = `${h}x${w}`;
+};
+
+saveButton.onclick = () => {
+    // const size = parseInt(sizeBar.value);
+    // sushi(size);
 };
